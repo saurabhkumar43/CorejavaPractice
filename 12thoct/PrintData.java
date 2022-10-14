eedb44862ad66355c09960d2d82fe49a8abb2253
saurabhkumar43 <115523278+saurabhkumar43@users.noreply.github.com>
2022-10-14 11:11:57 +0530

    Add files via upload

---
+++ 12thoct/PrintData.java
@@ -0,0 +1,10 @@
+class PrintData
+{
+	public static void main(String[] args)
+	{
+		System.out.println("FullName: "+ args[0]+ " " +args[1]);
+		System.out.println("Age: "+ args[2]);
+		System.out.println("course: "+ args[3]);
+		System.out.println("Score: "+ args[4]);
+	}
+}	
---
+++ 12thoct/PrintName.java
@@ -0,0 +1,7 @@
+class PrintName
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome "+ args[0]+ " " +args[1]);
+	}
+}	
