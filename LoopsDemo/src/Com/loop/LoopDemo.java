d3a70d8ce8e06dd0c119d36b173f03509e0dad86
saurabh kumar <saurabhbabubaseta9988h@gmail.com>
2022-10-25 15:54:39 +0530

    demo code committed

---
+++ LoopsDemo/src/Com/loop/LoopDemo.java
@@ -0,0 +1,25 @@
+/*
+This is a demo class to test package
+@author: Saurabh Kumar
+@Date: 25th oct 2022  
+*/
+
+//creating a pacakge
+package Com.loop;
+
+//declaring a class
+public class LoopDemo {
+	// declaring a instance variable
+	int myValue = 15;
+
+	// method to print value
+	public void printValue() {
+		System.out.println("Value is :" + myValue);
+	}
+
+	// static method to print message
+	public static void printMessage() {
+		System.out.println("Welcome");
+	}
+
+}// end of class
